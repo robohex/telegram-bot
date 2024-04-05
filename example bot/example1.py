761cb04360505ad1a65469669bedd042a985843c
robohex <166114467+robohex@users.noreply.github.com>
2024-04-05 21:54:52 +0400

    Update example1.py

--- example bot/example1.py
+++ example bot/example1.py
@@ -1,1 +1,57 @@
+import logging
 
+from telegram import ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("start massage here", callback_data="button_data")]]
+    )
+    await update.message.reply_html(
+        rf"differgem, oyun botudur.Bu bot en cok kullanilan eglence amacli kullanilan oyun botudur. Bu bot sayesinde qrupunuzdaki kullanicilari aktif tuta bilirsiniz.",
+        reply_markup=reply_markup,
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help message here!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("BOT_TOKEN_HERE").build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
